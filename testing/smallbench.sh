0b9d7abe6d3a060c3f80232eda5f52d24dfa7dcf
Feign Death <gord.lame@hotmail.de>
2024-04-19 13:22:08 +0200

    rearrangement

--- testing/smallbench.sh
+++ testing/smallbench.sh
@@ -1,26 +1,37 @@
-# python testing/benchmark-flatland.py -e encodings/multiple/grid/combined.lp -a 5
-# python testing/benchmark-flatland.py -e encodings/multiple/grid/combined.lp -a 10
-# python testing/benchmark-flatland.py -e encodings/multiple/grid/combined.lp -a 15
-# python testing/benchmark-flatland.py -e encodings/multiple/grid/combined.lp -a 20
-# python testing/benchmark-flatland.py -e encodings/multiple/grid/combined.lp -a 25
-# python testing/benchmark-flatland.py -e encodings/multiple/grid/combined.lp -a 30
-# python testing/benchmark-flatland.py -e encodings/multiple/grid/combined.lp -a 35
-# python testing/benchmark-flatland.py -e encodings/multiple/grid/combined.lp -a 40
+python testing/benchmark-flatland.py -e encodings/multiple/grid/combined.lp -m 20 -t 1200 -a 5
+python testing/benchmark-flatland.py -e encodings/multiple/grid/combined.lp -m 20 -t 1200 -a 10
+python testing/benchmark-flatland.py -e encodings/multiple/grid/combined.lp -m 20 -t 1200 -a 15
+python testing/benchmark-flatland.py -e encodings/multiple/grid/combined.lp -m 20 -t 1200 -a 20
+python testing/benchmark-flatland.py -e encodings/multiple/grid/combined.lp -m 20 -t 1200 -a 25
+python testing/benchmark-flatland.py -e encodings/multiple/grid/combined.lp -m 20 -t 1200 -a 30
+python testing/benchmark-flatland.py -e encodings/multiple/grid/combined.lp -m 20 -t 1200 -a 35
+python testing/benchmark-flatland.py -e encodings/multiple/grid/combined.lp -m 20 -t 1200 -a 40
 
-# python testing/benchmark-flatland.py -e encodings/multiple/weighted/combined.lp -a 5
-# python testing/benchmark-flatland.py -e encodings/multiple/weighted/combined.lp -a 10
-# python testing/benchmark-flatland.py -e encodings/multiple/weighted/combined.lp -a 15
-# python testing/benchmark-flatland.py -e encodings/multiple/weighted/combined.lp -a 20
-# python testing/benchmark-flatland.py -e encodings/multiple/weighted/combined.lp -a 25
-# python testing/benchmark-flatland.py -e encodings/multiple/weighted/combined.lp -a 30
-# python testing/benchmark-flatland.py -e encodings/multiple/weighted/combined.lp -a 35
-# python testing/benchmark-flatland.py -e encodings/multiple/weighted/combined.lp -a 40
+python testing/benchmark-flatland.py -e encodings/multiple/weighted/combined.lp -m 20 -t 1200 -a 5
+python testing/benchmark-flatland.py -e encodings/multiple/weighted/combined.lp -m 20 -t 1200 -a 10
+python testing/benchmark-flatland.py -e encodings/multiple/weighted/combined.lp -m 20 -t 1200 -a 15
+python testing/benchmark-flatland.py -e encodings/multiple/weighted/combined.lp -m 20 -t 1200 -a 20
+python testing/benchmark-flatland.py -e encodings/multiple/weighted/combined.lp -m 20 -t 1200 -a 25
+python testing/benchmark-flatland.py -e encodings/multiple/weighted/combined.lp -m 20 -t 1200 -a 30
+python testing/benchmark-flatland.py -e encodings/multiple/weighted/combined.lp -m 20 -t 1200 -a 35
+python testing/benchmark-flatland.py -e encodings/multiple/weighted/combined.lp -m 20 -t 1200 -a 40
 
-# python testing/benchmark-flatland.py -e encodings/multiple/ordered/combined.lp -a 5
-# python testing/benchmark-flatland.py -e encodings/multiple/ordered/combined.lp -a 10
-# python testing/benchmark-flatland.py -e encodings/multiple/ordered/combined.lp -a 15
-# python testing/benchmark-flatland.py -e encodings/multiple/ordered/combined.lp -a 20
-# python testing/benchmark-flatland.py -e encodings/multiple/ordered/combined.lp -a 25
-# python testing/benchmark-flatland.py -e encodings/multiple/ordered/combined.lp -a 30
-python testing/benchmark-flatland.py -e encodings/multiple/ordered/combined.lp -a 35
-python testing/benchmark-flatland.py -e encodings/multiple/ordered/combined.lp -a 40
+python testing/benchmark-flatland.py -e encodings/multiple/ordered/combined.lp -m 20 -t 1200 -a 5
+python testing/benchmark-flatland.py -e encodings/multiple/ordered/combined.lp -m 20 -t 1200 -a 10
+python testing/benchmark-flatland.py -e encodings/multiple/ordered/combined.lp -m 20 -t 1200 -a 15
+python testing/benchmark-flatland.py -e encodings/multiple/ordered/combined.lp -m 20 -t 1200 -a 20
+python testing/benchmark-flatland.py -e encodings/multiple/ordered/combined.lp -m 20 -t 1200 -a 25
+python testing/benchmark-flatland.py -e encodings/multiple/ordered/combined.lp -m 20 -t 1200 -a 30
+python testing/benchmark-flatland.py -e encodings/multiple/ordered/combined.lp -m 20 -t 1200 -a 35
+python testing/benchmark-flatland.py -e encodings/multiple/ordered/combined.lp -m 20 -t 1200 -a 40
+
+python testing/benchmark-flatland.py -e encodings/multiple/ordered_pipe/combined.lp -m 20 -t 1200 -a 5
+python testing/benchmark-flatland.py -e encodings/multiple/ordered_pipe/combined.lp -m 20 -t 1200 -a 10
+python testing/benchmark-flatland.py -e encodings/multiple/ordered_pipe/combined.lp -m 20 -t 1200 -a 15
+python testing/benchmark-flatland.py -e encodings/multiple/ordered_pipe/combined.lp -m 20 -t 1200 -a 20
+python testing/benchmark-flatland.py -e encodings/multiple/ordered_pipe/combined.lp -m 20 -t 1200 -a 25
+python testing/benchmark-flatland.py -e encodings/multiple/ordered_pipe/combined.lp -m 20 -t 1200 -a 30
+python testing/benchmark-flatland.py -e encodings/multiple/ordered_pipe/combined.lp -m 20 -t 1200 -a 35
+python testing/benchmark-flatland.py -e encodings/multiple/ordered_pipe/combined.lp -m 20 -t 1200 -a 40
+python testing/benchmark-flatland.py -e encodings/multiple/ordered_pipe/combined.lp -m 20 -t 1200 -a 45
+python testing/benchmark-flatland.py -e encodings/multiple/ordered_pipe/combined.lp -m 20 -t 1200 -a 50
