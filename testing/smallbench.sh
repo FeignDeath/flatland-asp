975491a2ab7d87ab705774ac8761f829b0acf489
Feign Death <feign_death@tutanota.de>
2024-05-02 16:34:53 +0200

    update results

--- testing/smallbench.sh
+++ testing/smallbench.sh
@@ -65,7 +65,6 @@ python testing/benchmark-flatland.py -e encodings/multiple/weighted/ -x 100 -y 1
 python testing/benchmark-flatland.py -e encodings/multiple/weighted/ -x 100 -y 100 -m 20 -f 4 -t 600 -a 50
 python testing/benchmark-flatland.py -e encodings/multiple/weighted/ -x 100 -y 100 -m 20 -f 4 -t 600 -a 55
 python testing/benchmark-flatland.py -e encodings/multiple/weighted/ -x 100 -y 100 -m 20 -f 4 -t 600 -a 60
-python testing/benchmark-flatland.py -e encodings/multiple/weighted/ -x 100 -y 100 -m 20 -f 4 -t 600 -a 65
 
 python testing/benchmark-flatland.py -e encodings/multiple/ordered/ -x 100 -y 100 -m 20 -f 4 -t 600 -a 5
 python testing/benchmark-flatland.py -e encodings/multiple/ordered/ -x 100 -y 100 -m 20 -f 4 -t 600 -a 10
@@ -88,4 +87,37 @@ python testing/benchmark-flatland.py -e encodings/multiple/ordered_pipe/ -x 100 
 python testing/benchmark-flatland.py -e encodings/multiple/ordered_pipe/ -x 100 -y 100 -m 20 -f 4 -t 600 -a 65
 python testing/benchmark-flatland.py -e encodings/multiple/ordered_pipe/ -x 100 -y 100 -m 20 -f 4 -t 600 -a 70
 
-# shutdown
+python testing/benchmark-flatland.py -e encodings/multiple/grid/ -x 200 -y 200 -m 20 -f 4 -t 600 -a 5
+python testing/benchmark-flatland.py -e encodings/multiple/grid/ -x 200 -y 200 -m 20 -f 4 -t 600 -a 10
+python testing/benchmark-flatland.py -e encodings/multiple/grid/ -x 200 -y 200 -m 20 -f 4 -t 600 -a 15
+
+python testing/benchmark-flatland.py -e encodings/multiple/weighted/ -x 200 -y 200 -m 20 -f 4 -t 600 -a 5
+python testing/benchmark-flatland.py -e encodings/multiple/weighted/ -x 200 -y 200 -m 20 -f 4 -t 600 -a 10
+python testing/benchmark-flatland.py -e encodings/multiple/weighted/ -x 200 -y 200 -m 20 -f 4 -t 600 -a 15
+python testing/benchmark-flatland.py -e encodings/multiple/weighted/ -x 200 -y 200 -m 20 -f 4 -t 600 -a 20
+python testing/benchmark-flatland.py -e encodings/multiple/weighted/ -x 200 -y 200 -m 20 -f 4 -t 600 -a 25
+python testing/benchmark-flatland.py -e encodings/multiple/weighted/ -x 200 -y 200 -m 20 -f 4 -t 600 -a 30
+python testing/benchmark-flatland.py -e encodings/multiple/weighted/ -x 200 -y 200 -m 20 -f 4 -t 600 -a 35
+python testing/benchmark-flatland.py -e encodings/multiple/weighted/ -x 200 -y 200 -m 20 -f 4 -t 600 -a 40
+python testing/benchmark-flatland.py -e encodings/multiple/weighted/ -x 200 -y 200 -m 20 -f 4 -t 600 -a 45
+python testing/benchmark-flatland.py -e encodings/multiple/weighted/ -x 200 -y 200 -m 20 -f 4 -t 600 -a 50
+
+python testing/benchmark-flatland.py -e encodings/multiple/ordered/ -x 200 -y 200 -m 20 -f 4 -t 600 -a 5
+python testing/benchmark-flatland.py -e encodings/multiple/ordered/ -x 200 -y 200 -m 20 -f 4 -t 600 -a 10
+python testing/benchmark-flatland.py -e encodings/multiple/ordered/ -x 200 -y 200 -m 20 -f 4 -t 600 -a 15
+python testing/benchmark-flatland.py -e encodings/multiple/ordered/ -x 200 -y 200 -m 20 -f 4 -t 600 -a 20
+
+python testing/benchmark-flatland.py -e encodings/multiple/ordered_pipe/ -x 200 -y 200 -m 20 -f 4 -t 600 -a 5
+python testing/benchmark-flatland.py -e encodings/multiple/ordered_pipe/ -x 200 -y 200 -m 20 -f 4 -t 600 -a 10
+python testing/benchmark-flatland.py -e encodings/multiple/ordered_pipe/ -x 200 -y 200 -m 20 -f 4 -t 600 -a 15
+python testing/benchmark-flatland.py -e encodings/multiple/ordered_pipe/ -x 200 -y 200 -m 20 -f 4 -t 600 -a 20
+python testing/benchmark-flatland.py -e encodings/multiple/ordered_pipe/ -x 200 -y 200 -m 20 -f 4 -t 600 -a 25
+python testing/benchmark-flatland.py -e encodings/multiple/ordered_pipe/ -x 200 -y 200 -m 20 -f 4 -t 600 -a 30
+python testing/benchmark-flatland.py -e encodings/multiple/ordered_pipe/ -x 200 -y 200 -m 20 -f 4 -t 600 -a 35
+python testing/benchmark-flatland.py -e encodings/multiple/ordered_pipe/ -x 200 -y 200 -m 20 -f 4 -t 600 -a 40
+python testing/benchmark-flatland.py -e encodings/multiple/ordered_pipe/ -x 200 -y 200 -m 20 -f 4 -t 600 -a 45
+python testing/benchmark-flatland.py -e encodings/multiple/ordered_pipe/ -x 200 -y 200 -m 20 -f 4 -t 600 -a 50
+python testing/benchmark-flatland.py -e encodings/multiple/ordered_pipe/ -x 200 -y 200 -m 20 -f 4 -t 600 -a 55
+python testing/benchmark-flatland.py -e encodings/multiple/ordered_pipe/ -x 200 -y 200 -m 20 -f 4 -t 600 -a 60
+
+shutdown
