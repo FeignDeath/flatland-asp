53cffdc4643dd6357647e2e986c9b800b39345e0
Feign Death <gord.lame@hotmail.de>
2024-05-09 01:38:32 +0200

    benchmark and evaluation

--- testing/smallbench.sh
+++ testing/smallbench.sh
@@ -44,6 +44,16 @@ python testing/benchmark-flatland.py -e encodings/multiple/ordered_pipe/ -x 50 -
 python testing/benchmark-flatland.py -e encodings/multiple/ordered_pipe/ -x 50 -y 50 -m 20 -f 4 -t 600 -a 95
 python testing/benchmark-flatland.py -e encodings/multiple/ordered_pipe/ -x 50 -y 50 -m 20 -f 4 -t 600 -a 100
 
+python testing/benchmark-flatland.py -e encodings/multiple/path_assigning/ -x 50 -y 50 -m 20 -f 4 -t 600 -a 5
+python testing/benchmark-flatland.py -e encodings/multiple/path_assigning/ -x 50 -y 50 -m 20 -f 4 -t 600 -a 10
+python testing/benchmark-flatland.py -e encodings/multiple/path_assigning/ -x 50 -y 50 -m 20 -f 4 -t 600 -a 15
+python testing/benchmark-flatland.py -e encodings/multiple/path_assigning/ -x 50 -y 50 -m 20 -f 4 -t 600 -a 20
+python testing/benchmark-flatland.py -e encodings/multiple/path_assigning/ -x 50 -y 50 -m 20 -f 4 -t 600 -a 25
+python testing/benchmark-flatland.py -e encodings/multiple/path_assigning/ -x 50 -y 50 -m 20 -f 4 -t 600 -a 30
+python testing/benchmark-flatland.py -e encodings/multiple/path_assigning/ -x 50 -y 50 -m 20 -f 4 -t 600 -a 35
+python testing/benchmark-flatland.py -e encodings/multiple/path_assigning/ -x 50 -y 50 -m 20 -f 4 -t 600 -a 40
+python testing/benchmark-flatland.py -e encodings/multiple/path_assigning/ -x 50 -y 50 -m 20 -f 4 -t 600 -a 45
+
 python testing/benchmark-flatland.py -e encodings/multiple/ordered_pipe_minimize/ -x 50 -y 50 -m 20 -f 4 -t 600 -a 5
 python testing/benchmark-flatland.py -e encodings/multiple/ordered_pipe_minimize/ -x 50 -y 50 -m 20 -f 4 -t 600 -a 10
 
@@ -87,6 +97,16 @@ python testing/benchmark-flatland.py -e encodings/multiple/ordered_pipe/ -x 100 
 python testing/benchmark-flatland.py -e encodings/multiple/ordered_pipe/ -x 100 -y 100 -m 20 -f 4 -t 600 -a 65
 python testing/benchmark-flatland.py -e encodings/multiple/ordered_pipe/ -x 100 -y 100 -m 20 -f 4 -t 600 -a 70
 
+python testing/benchmark-flatland.py -e encodings/multiple/path_assigning/ -x 100 -y 100 -m 20 -f 4 -t 600 -a 5
+python testing/benchmark-flatland.py -e encodings/multiple/path_assigning/ -x 100 -y 100 -m 20 -f 4 -t 600 -a 10
+python testing/benchmark-flatland.py -e encodings/multiple/path_assigning/ -x 100 -y 100 -m 20 -f 4 -t 600 -a 15
+python testing/benchmark-flatland.py -e encodings/multiple/path_assigning/ -x 100 -y 100 -m 20 -f 4 -t 600 -a 20
+python testing/benchmark-flatland.py -e encodings/multiple/path_assigning/ -x 100 -y 100 -m 20 -f 4 -t 600 -a 25
+python testing/benchmark-flatland.py -e encodings/multiple/path_assigning/ -x 100 -y 100 -m 20 -f 4 -t 600 -a 30
+python testing/benchmark-flatland.py -e encodings/multiple/path_assigning/ -x 100 -y 100 -m 20 -f 4 -t 600 -a 35
+python testing/benchmark-flatland.py -e encodings/multiple/path_assigning/ -x 100 -y 100 -m 20 -f 4 -t 600 -a 40
+python testing/benchmark-flatland.py -e encodings/multiple/path_assigning/ -x 100 -y 100 -m 20 -f 4 -t 600 -a 45
+
 python testing/benchmark-flatland.py -e encodings/multiple/grid/ -x 200 -y 200 -m 20 -f 4 -t 600 -a 5
 python testing/benchmark-flatland.py -e encodings/multiple/grid/ -x 200 -y 200 -m 20 -f 4 -t 600 -a 10
 python testing/benchmark-flatland.py -e encodings/multiple/grid/ -x 200 -y 200 -m 20 -f 4 -t 600 -a 15
@@ -119,5 +139,15 @@ python testing/benchmark-flatland.py -e encodings/multiple/ordered_pipe/ -x 200 
 python testing/benchmark-flatland.py -e encodings/multiple/ordered_pipe/ -x 200 -y 200 -m 20 -f 4 -t 600 -a 50
 python testing/benchmark-flatland.py -e encodings/multiple/ordered_pipe/ -x 200 -y 200 -m 20 -f 4 -t 600 -a 55
 python testing/benchmark-flatland.py -e encodings/multiple/ordered_pipe/ -x 200 -y 200 -m 20 -f 4 -t 600 -a 60
+
+python testing/benchmark-flatland.py -e encodings/multiple/path_assigning/ -x 200 -y 200 -m 20 -f 4 -t 600 -a 5
+python testing/benchmark-flatland.py -e encodings/multiple/path_assigning/ -x 200 -y 200 -m 20 -f 4 -t 600 -a 10
+python testing/benchmark-flatland.py -e encodings/multiple/path_assigning/ -x 200 -y 200 -m 20 -f 4 -t 600 -a 15
+python testing/benchmark-flatland.py -e encodings/multiple/path_assigning/ -x 200 -y 200 -m 20 -f 4 -t 600 -a 20
+python testing/benchmark-flatland.py -e encodings/multiple/path_assigning/ -x 200 -y 200 -m 20 -f 4 -t 600 -a 25
+python testing/benchmark-flatland.py -e encodings/multiple/path_assigning/ -x 200 -y 200 -m 20 -f 4 -t 600 -a 30
+python testing/benchmark-flatland.py -e encodings/multiple/path_assigning/ -x 200 -y 200 -m 20 -f 4 -t 600 -a 35
+python testing/benchmark-flatland.py -e encodings/multiple/path_assigning/ -x 200 -y 200 -m 20 -f 4 -t 600 -a 40
+python testing/benchmark-flatland.py -e encodings/multiple/path_assigning/ -x 200 -y 200 -m 20 -f 4 -t 600 -a 45
 
 shutdown
